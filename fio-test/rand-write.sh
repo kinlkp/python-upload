c954f4ed38d36a0db0f635dde8fa463294a5a22c
brian <brian.leungkp@gmail.com>
2025-07-06 23:45:24 +0800

    add fio

---
+++ fio-test/rand-write.sh
@@ -0,0 +1,1 @@
+fio --name=seqwrite --rw=randrw --direct=1 --ioengine=libaio --bs=4k --numjobs=1 --size=1000M
---
+++ fio-test/seq-write.sh
@@ -0,0 +1,1 @@
+fio --name=seqwrite --rw=write --direct=1 --ioengine=libaio --bs=4k --numjobs=1 --size=1000M
