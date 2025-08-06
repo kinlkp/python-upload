9cabb5278ddd4a6d0321ec07adac15ef777a6011
brian <brian.leungkp@gmail.com>
2025-08-06 21:49:57 +0800

    updates

--- sitescope/check-win-after-patching.ps1
+++
@@ -1,8 +0,0 @@
-# $hf = Get-HotFix | Sort-Object -Descending -Property InstalledOn
-$hf = Get-WindowsPackage -Online | Sort-Object -Descending -Property InstallTime
-
-foreach ($h in $hf) {
-    $d = $($h.InstallTime).ToString()
-    $state = $($h.PackageState)
-    Write-Host "$d => $state"
-}
